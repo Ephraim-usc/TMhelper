2cfb41c60dba51a926dca81b758cfe4a587fdc1c
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-06-30 13:56:00 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import numpy as np
 
+orders = pd.read_csv("orders.csv")
+
+def buyer_show(buyers, uid):
+  idx = np.where(buyers.loc[:, "uid"] == uid)[0] # find uid matches
+  if idx.shape[0] > 1:
+    print("Error: multiple matching")
+  else:
+    idx = idx[0] # get the unique match
+  buyer = buyers.iloc[idx, :]
+  print(buyer)
+  return buyer

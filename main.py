7ef35371a18d727fc6eda912de1d7bc440c76d10
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-06-30 12:42:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from tkinter import *
 from tkinter import ttk
 
 def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
-    except ValueError:
-        pass
+  try:
+    value = float(feet.get())
+    meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+  except ValueError:
+    pass
 
 root = Tk()
 root.title("Feet to Meters")

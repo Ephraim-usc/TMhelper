8c6749c4b675555c14543b7ce2c6a5080f3363aa
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-06-30 16:53:24 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -11,3 +11,21 @@ def get_buyer(buyers, uid):
     idx = idx[0] # get the unique match
   buyer = buyers.iloc[idx, :]
   return buyer
+
+def get_product(products, uid):
+  idx = np.where(products.loc[:, "uid"] == uid)[0] # find uid matches
+  if idx.shape[0] > 1:
+    print("Error: multiple matching")
+  else:
+    idx = idx[0] # get the unique match
+  product = products.iloc[idx, :]
+  return product
+
+def get_order(orders, uid):
+  idx = np.where(orders.loc[:, "uid"] == uid)[0] # find uid matches
+  if idx.shape[0] > 1:
+    print("Error: multiple matching")
+  else:
+    idx = idx[0] # get the unique match
+  order = orders.iloc[idx, :]
+  return order

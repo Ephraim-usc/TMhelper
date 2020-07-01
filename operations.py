f6fcbf758bf43ba9f27aefd9a590c06a96893f41
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-06-30 22:41:42 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -29,3 +29,26 @@ def get_order(orders, uid):
     idx = idx[0] # get the unique match
   order = orders.iloc[idx, :]
   return order
+
+def feed_gmail(str):
+  gmails = pd.read_csv("gmails.csv")
+  strll = [tmp.split() for tmp in str.split('\n')]
+  new = pd.DataFrame(strll, columns = gmails.columns)
+  gmails = pd.concat([gmails, new])
+  gmails.to_csv("gmails.csv")
+
+def feed_address(str):
+  addresses = pd.read_csv("addresses.csv")
+  strll = [tmp.split('\t') for tmp in str.split('\n')]
+  new = pd.DataFrame(strll, columns = addresses.columns)
+  addresses = pd.concat([addresses, new])
+  addresses.to_csv("addresses.csv")
+
+def feed_bankcards(str):
+  bankcards = pd.read_csv("bankcards.csv")
+  strll = [tmp.split('\t') for tmp in str.split('\n')]
+  new = pd.DataFrame(strll, columns = bankcards.columns)
+  bankcards = pd.concat([bankcards, new])
+  bankcards.to_csv("bankcards.csv")
+  
+  

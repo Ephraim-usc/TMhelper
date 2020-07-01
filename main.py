6f7cf2047dc9d87844ed4c9725d59e9ab6f7cdc2
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-07-01 08:32:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
+import operations as op
+
 ### root
 
 root = tk.Tk()
@@ -37,20 +39,14 @@ feed_text.place(x = 30, y = 30, width = 600, height = 400)
 def feed_submit_event():
   str = feed_text.get("1.0","end-1c")
   feed_text.delete('1.0', tk.END)
-  if feed.get() == "Gmails":
-    feed_gmails(str)
-  elif feed.get() == "Addresses":
-    feed_addresses(str)
-  elif feed.get() == "BankCards":
-    feed_bankcards(str)
+  datatype = {"Gmails":"gmail", "Addresses":"address", "BankCards":"bankcard"}[feed.get()]
+  op.feed(datatype, str)
 
 def feed_clear_event():
   feed_text.delete('1.0', tk.END)
 
 def feed_quit_event():
   feed.set("Import Data")
-
-feedframe.place(x = 0, y = 30)
 
 ttk.Button(feedframe, text="Submit", command=feed_submit_event).place(x = 650, y = 250, height = 30, width = 95)
 ttk.Button(feedframe, text="Clear", command=feed_clear_event).place(x = 650, y = 290, height = 30, width = 95)
@@ -77,7 +73,6 @@ phone_combobox.place(x = 650, y = 2, height = 40, width = 145)
 ### buyer
 
 buyerframe = ttk.Frame(root, width = 800, height = 470)
-buyerframe.place(x = 0, y = 30)
 
 gmail_text = tk.Text(buyerframe); gmail_text.place(x = 10, y = 10, width = 500, height = 120)
 address_text = tk.Text(buyerframe); address_text.place(x = 10, y = 140, width = 500, height = 150)
@@ -85,7 +80,7 @@ bankcard_text = tk.Text(buyerframe); bankcard_text.place(x = 10, y = 300, width 
 
 def buyer_event(event):
   buyerframe.place(x = 0, y = 30)
-  gmail, address, bankcard = new_buyer()
+  gmail, address, bankcard = op.new_buyer()
   gmail_text.delete(1.0,"end"); gmail_text.insert(1.0, str(gmail[2:])); 
   address_text.delete(1.0,"end"); address_text.insert(1.0, str(address[2:]))
   bankcard_text.delete(1.0,"end"); bankcard_text.insert(1.0, str(bankcard[2:]))
@@ -95,8 +90,18 @@ def buyer_event(event):
 
 buyer_button.bind('<Button-1>', buyer_event)
 
+### admin
 
-91104-4618
+adminframe = ttk.Frame(root, width = 800, height = 470)
+adminframe.place(x = 0, y = 30)
+search_text = tk.Text(adminframe); search_text.place(x = 50, y = 50, width = 400, height = 25)
+search_combobox = ttk.Combobox(adminframe); search_combobox.place(x = 460, y = 50, width = 100, height = 30)
+search_combobox['values'] = ['Gmails', 'Addresses', 'BankCards']
+search_combobox.current(0)
+search_button = ttk.Button(adminframe, text = "Search"); search_button.place(x = 570, y = 50, width = 100, height = 30)
+search_listbox = tk.Listbox(adminframe); search_listbox.place(x = 50, y = 100, width = 600, height = 300)
+
+
 
 #root.mainloop()
 

9843232ada61f63322e11e32aed02753c422cb7c
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-07-01 00:56:58 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-
-orders = pd.read_csv("orders.csv")
+import os
 
 def get_buyer(buyers, uid):
+  buyers = pd.read_csv("buyers.csv")
   idx = np.where(buyers.loc[:, "uid"] == uid)[0] # find uid matches
   if idx.shape[0] > 1:
     print("Error: multiple matching")
@@ -13,6 +13,7 @@ def get_buyer(buyers, uid):
   return buyer
 
 def get_product(products, uid):
+  products = pd.read_csv("products.csv")
   idx = np.where(products.loc[:, "uid"] == uid)[0] # find uid matches
   if idx.shape[0] > 1:
     print("Error: multiple matching")
@@ -22,6 +23,7 @@ def get_product(products, uid):
   return product
 
 def get_order(orders, uid):
+  orders = pd.read_csv("orders.csv")
   idx = np.where(orders.loc[:, "uid"] == uid)[0] # find uid matches
   if idx.shape[0] > 1:
     print("Error: multiple matching")
@@ -30,25 +32,55 @@ def get_order(orders, uid):
   order = orders.iloc[idx, :]
   return order
 
-def feed_gmail(str):
-  gmails = pd.read_csv("gmails.csv")
+def feed_gmails(str):
+  columns = pd.Index(['uid', 'buyer', 'Gmail', 'Password', 'SupportGmail', 'SupportGmailPassword'])
+  if os.path.isfile("gmails.csv"):
+    gmails = pd.read_csv("gmails.csv", index_col=0)
+  else:
+    gmails = pd.DataFrame(columns = columns)
+  
   strll = [tmp.split() for tmp in str.split('\n')]
-  new = pd.DataFrame(strll, columns = gmails.columns)
+  new = pd.DataFrame(strll, columns = columns[2:])
+  
+  uids = np.sort(list(set(range(gmails.shape[0] + new.shape[0])) - set(gmails.uid)))[:new.shape[0]]
+  new["uid"] = uids; new["buyer"] = np.nan
   gmails = pd.concat([gmails, new])
   gmails.to_csv("gmails.csv")
 
-def feed_address(str):
-  addresses = pd.read_csv("addresses.csv")
+def feed_addresses(str):
+  columns = pd.Index(['uid', 'buyer', 'RecipientName', 'Address1', 'Address2', 'City', 'Zip', 'State', 'PhoneNumber'])
+  if os.path.isfile("addresses.csv"):
+    addresses = pd.read_csv("addresses.csv", index_col=0)
+  else:
+    addresses = pd.DataFrame(columns = columns)
+  
   strll = [tmp.split('\t') for tmp in str.split('\n')]
-  new = pd.DataFrame(strll, columns = addresses.columns)
+  new = pd.DataFrame(strll, columns = columns[2:])
+  
+  uids = np.sort(list(set(range(addresses.shape[0] + new.shape[0])) - set(addresses.uid)))[:new.shape[0]]
+  new["uid"] = uids; new["buyer"] = np.nan
   addresses = pd.concat([addresses, new])
   addresses.to_csv("addresses.csv")
 
 def feed_bankcards(str):
-  bankcards = pd.read_csv("bankcards.csv")
+  columns = pd.Index(['uid', 'buyer', 'BankNumber', 'BankCard', 'BankCardExpirationDate'])
+  if os.path.isfile("bankcards.csv"):
+    bankcards = pd.read_csv("bankcards.csv", index_col=0)
+  else:
+    bankcards = pd.DataFrame(columns = columns)
+  
   strll = [tmp.split('\t') for tmp in str.split('\n')]
-  new = pd.DataFrame(strll, columns = bankcards.columns)
+  new = pd.DataFrame(strll, columns = columns[2:])
+  
+  uids = np.sort(list(set(range(bankcards.shape[0] + new.shape[0])) - set(bankcards.uid)))[:new.shape[0]]
+  new["uid"] = uids; new["buyer"] = np.nan
   bankcards = pd.concat([bankcards, new])
   bankcards.to_csv("bankcards.csv")
-  
-  
+
+def new_buyer():
+  gmails = pd.read_csv("gmails.csv", index_col=0); gmail = gmails.iloc[np.where(gmails.buyer != gmails.buyer)[0][0], :]
+  addresses = pd.read_csv("addresses.csv", index_col=0); address = addresses.iloc[np.where(addresses.buyer != addresses.buyer)[0][0], :]
+  bankcards = pd.read_csv("bankcards.csv", index_col=0); bankcard = bankcards.iloc[np.where(addresses.buyer != addresses.buyer)[0][0], :]
+  return gmail, address, bankcard
+
+#def new_buyer_submit():

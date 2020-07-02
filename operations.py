2d71bb99b18ebcb08a99861e3a84df41be0ee9d4
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-07-01 23:59:53 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -1,81 +1,132 @@
 import pandas as pd
 import numpy as np
 import os
+import pickle
 
-'''
-def get_buyer(buyers, uid):
-  buyers = pd.read_csv("buyers.csv")
-  idx = np.where(buyers.loc[:, "uid"] == uid)[0] # find uid matches
-  if idx.shape[0] > 1:
-    print("Error: multiple matching")
-  else:
-    idx = idx[0] # get the unique match
-  buyer = buyers.iloc[idx, :]
-  return buyer
+class entry():
+  attributes = ["uid", "note"]
+  values = [None, None]
+  _initials = []
+  
+  def __init__(self, data): # to initialize an entry, input a list of its attributes
+    data = self._initials + data
+    self.values = data
+    self.dict = dict(zip(self.attributes, self.values))
+  
+  def str(self):
+    zipped = zip(self.attributes, self.values)
+    buffer = '\n'.join([attribute+"\t"+str(value) for attribute, value in zipped])
+    return buffer
+  
+  def get(self, attribute):
+    buffer = self.dict[attribute]
+    return buffer
+  
+  def set(self, attribute, new):
+    self.dict[attribute] = new
+    self.values = self.dict.values()
 
-'''
+class gmail(entry):
+  filename = "gmails.p"
+  attributes = entry.attributes + ['buyers', 'Gmail', 'Password', 'SupportGmail', 'SupportGmailPassword']
+  _initials = [None, None, None]
+  
+  def symbol(self):
+    buffer = "<" + self.get("Gmail") + ">"
+    return buffer
 
-### lookup table for all datatypes
-gmail_columns = ['uid', 'buyer', 'Gmail', 'Password', 'SupportGmail', 'SupportGmailPassword']
-gmail_filename = "gmails.csv"
-gmail_str = lambda x: "<" + x["Gmail"] + ">"
-gmail_lkt = {"columns":gmail_columns, "filename":gmail_filename, "str_method":gmail_str}
 
-address_columns = ['uid', 'buyer', 'RecipientName', 'Address1', 'Address2', 'City', 'Zip', 'State', 'PhoneNumber']
-address_filename = "addresses.csv"
-address_lkt = {"columns":address_columns, "filename":address_filename}
+class address(entry):
+  filename = "address.p"
+  attributes = entry.attributes + ['buyers', 'RecipientName', 'Address1', 'Address2', 'City', 'Zip', 'State', 'PhoneNumber']
+  _initials = [None, None, None]
+  
+  def symbol(self):
+    buffer = "<" + self.get("RecipientName") + ">"
+    return buffer
 
-bankcard_columns = ['uid', 'buyer', 'BankNumber', 'BankCard', 'BankCardExpirationDate']
-bankcard_filename = "bankcards.csv"
-bankcard_str = lambda x: "<" + x["BankCard"] + ">"
-bankcard_lkt = {"columns":bankcard_columns, "filename":bankcard_filename, "str_method":bankcard_str}
 
-lkt = {"gmail":gmail_lkt, "address":address_lkt, "bankcard":bankcard_lkt}
+class bankcard(entry):
+  filename = "bankcards.p"
+  attributes = entry.attributes + ['buyers', 'BankNumber', 'BankCard', 'BankCardExpirationDate']
+  _initials = [None, None, None]
+  
+  def symbol(self):
+    buffer = "<" + self.get("BankCard") + ">"
+    return buffer
 
-###
+class entryList():
+  datatype = entry
+  values = []
+  
+  def __init__(self, el): # el is a list of entries
+    if not el == []:
+      self.datatype = type(el[0])
+    self.values = el
+  
+  def get(self, attribute):
+    buffer = [x.get(attribute) for x in self.values]
+    return buffer
+  
+  def set(self, attribute, new):
+    if len(new) != len(self.values):
+      print("error!")
+    for i in range(len(self.values)):
+      self.values[i].set(attribute, new[i])
+  
+  def append(self, e):
+    self.values = self.values + [e]
+
+
+def entryList_load(filename):
+  buffer = pickle.load(open(filename, "rb"))
+  return buffer
+
+def entryList_Write(x, filename):
+  pickle.dump(x, open(filename, "wb"))
+
+def entryList_from_string(datatype, string): # string is \t separated and \n carriage returned  # 需要检验格式
+  stringll = [tmp.split('\t') for tmp in string.split('\n')]
+  el = [datatype(x) for x in stringll]
+  buffer = entryList(el)
+  return buffer
+
+def entryList_merge(x, y):
+  buffer = entryList([]); buffer.datatype = x.datatype
+  for e in x.values:
+    buffer.append(e)
+  for e in y.values:
+    buffer.append(e)
+  return buffer
+
+### special functionalities
+
 def feed(datatype, string):
-  filename = lkt[datatype]["filename"]
-  columns = lkt[datatype]["columns"]
+  filename = datatype.filename
   if os.path.isfile(filename):
-    current = pd.read_csv(filename, index_col=0, dtype = str)
+    current = entryList_Load(filename)
   else:
-    current = pd.DataFrame(columns = columns)
+    current = entryList([])
+    current.datatype = datatype
   
-  new = pd.DataFrame([tmp.split('\t') for tmp in string.split('\n')], 
-                     columns = list(set(columns) - set(["uid", "buyer"])))
-  total_nrow = current.shape[0] + new.shape[0]
-  current_uids = np.array(current.uid).astype(int)
-  uids = np.sort(list(set(range(total_nrow)) - set(current_uids)))[:new.shape[0]]
-  new["uid"] = uids; new["buyer"] = np.nan
-  merged = pd.concat([current, new])
-  merged.to_csv(filename)
-
-def new_buyer():
-  gmails = pd.read_csv("gmails.csv", index_col=0, dtype = str); gmail = gmails.iloc[np.where(gmails.buyer != gmails.buyer)[0][0], :]
-  addresses = pd.read_csv("addresses.csv", index_col=0, dtype = str); address = addresses.iloc[np.where(addresses.buyer != addresses.buyer)[0][0], :]
-  bankcards = pd.read_csv("bankcards.csv", index_col=0, dtype = str); bankcard = bankcards.iloc[np.where(addresses.buyer != addresses.buyer)[0][0], :]
-  return gmail, address, bankcard
-
-
-
-#def new_buyer_submit():
-
-def search(datatype, string):
-  filename = lkt[datatype]["filename"]
-  data = pd.read_csv(filename, index_col=0, dtype= str)
-  str_method = lkt[datatype]["str_method"]
+  new = entryList_from_string(datatype, string)
+  maximum = len(current.values) + len(new.values)
+  uids = np.sort(list(set(range(maximum)) - set(current.get("uid"))))[: len(new.values)]
+  new.set("uid", uids)
   
+  merged = entryList_merge(current, new)
+  entryList_Write(merged, filename)
+
+def search(entrylist, string):
   buffer = []
-  for i in range(data.shape[0]):
-    entry = data.iloc[i, :]
+  for e in entrylist.values:
     match = False
-    for j in range(entry.shape[0]):
-      if not isinstance(entry[j], str):
-        continue
-      if string in entry[j]:
+    for key, value in e.dict.items():
+      if string in str(value): 
         match = True
+        break
     if match == True:
-      buffer.append(str_method(entry))
-  
-  return(buffer)
+      buffer.append((e, key))
+  return buffer
+
 

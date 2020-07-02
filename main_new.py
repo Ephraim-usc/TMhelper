17b4058408bc3da1190f76e0cdb64027beb46840
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-07-02 00:47:46 -0500

    Update main_new.py

--- main_new.py
+++ main_new.py
@@ -3,14 +3,27 @@ import tkinter.ttk as ttk
 
 import operations as op
 
-
+class TMhelper(tk.Tk):
+  def __init__(self, *args, **kwargs):
+    tk.Tk.__init__(self, *args, **kwargs)
+    self.title('TMhelper')
+    self.geometry("800x500")
+    
+    self.menuframe = Menu(self)
+    self.feedframe = Feed(self)
+    self.adminframe = Admin(self)
+    self.buyerframe = Buyer(self)
 
 class Menu(tk.Frame):
   def __init__(self, parent, *args, **kwargs):
     tk.Frame.__init__(self, parent, *args, **kwargs)
     self.parent = parent
     self.configure(width = 800, height = 30)
+    self.place(x= 0, y = 0)
     self['bg'] = 'grey'
+    
+    self.feed = tk.StringVar()
+    self.phone = tk.StringVar()
     
     self.login_button = ttk.Button(self, text="Login")
     self.login_button.place(x = 0, y = 0, height = 30, width = 95)
@@ -27,17 +40,27 @@ class Menu(tk.Frame):
     self.review_button = ttk.Button(self, text="Review")
     self.review_button.place(x = 400, y = 0, height = 30, width = 95)
     
-    global feed
-    self.feed_combobox = ttk.Combobox(self, textvariable = feed)
+    self.feed_combobox = ttk.Combobox(self, textvariable = self.feed)
     self.feed_combobox['values'] = ['Import Data', 'Gmails', 'Addresses', 'BankCards', 'Reviews']
     self.feed_combobox.current(0)
     self.feed_combobox.place(x = 500, y = 2, height = 40, width = 145)
     
-    global phone
-    self.phone_combobox = ttk.Combobox(self, textvariable = phone)
+    self.phone_combobox = ttk.Combobox(self, textvariable = self.phone)
     self.phone_combobox['values'] = ['Select Phone', 'iphone1', 'iphone2', 'android']
     self.phone_combobox.current(0)
     self.phone_combobox.place(x = 650, y = 2, height = 40, width = 145)
+    
+    self.feed.trace("w", self.feed_write_event)
+    
+  def feed_write_event(self, var, indx, mode):
+    parent = self.parent
+    other_frames = [w for w in parent.winfo_children() if w.winfo_y() > 0]
+    for w in other_frames: w.place_forget()
+    
+    if self.feed.get() == "Import Data":
+      parent.feedframe.place_forget()
+    else:
+      parent.feedframe.place(x = 0, y = 30)
 
 
 class Feed(tk.Frame):
@@ -56,8 +79,12 @@ class Feed(tk.Frame):
     self.clear_button = ttk.Button(self, text="Clear")
     self.clear_button.place(x = 650, y = 290, height = 30, width = 95)
     
-    self.quit_button = ttk.Button(self, text="Quit")
+    self.quit_button = ttk.Button(self, text="Quit", command = self.quit)
     self.quit_button.place(x = 650, y = 330, height = 30, width = 95)
+  
+  def quit(self):
+    self.place_forget()
+    
 
 class Admin(tk.Frame):
   def __init__(self, parent, *args, **kwargs):
@@ -79,27 +106,33 @@ class Admin(tk.Frame):
     
     self.search_listbox = tk.Listbox(self)
     self.search_listbox.place(x = 50, y = 100, width = 600, height = 300)
-
-
-root = tk.Tk()
-root.title('TMhelper')
-root.geometry("800x500")
-
-feed = tk.StringVar()
-phone = tk.StringVar()
-
-menuframe = Menu(root); menuframe.place(x = 0, y = 0)
-feedframe = Feed(root); feedframe.place(x = 0, y = 30)
-adminframe = Admin(root); adminframe.place(x = 0, y = 30)
-
-def feed_event(var, indx, mode):
-  other_frames = [w for w in root.winfo_children() if w.winfo_y() > 0]
-  for w in other_frames: w.place_forget()
+    
+    self.quit_button = ttk.Button(self, text="Quit", command = self.quit)
+    self.quit_button.place(x = 650, y = 330, height = 30, width = 95)
   
-  if feed.get() == "Import Data":
-    feedframe.place_forget()
-  else:
-    feedframe.place(x = 0, y = 30)
+  def quit(self):
+    self.place_forget()
 
-feed.trace("w", feed_event)
+class Buyer(tk.Frame):
+  def __init__(self, parent, *args, **kwargs):
+    tk.Frame.__init__(self, parent, *args, **kwargs)
+    self.parent = parent
+    self.configure(width = 800, height = 470)
+    self['bg'] = 'light yellow'
+    
+    self.attributes_text = tk.Text(self); 
+    self.attributes_text.place(x = 50, y = 50, width = 150, height = 300)
+    self.attributes_text.configure(state = "disabled")
+    
+    self.values_text = tk.Text(self); 
+    self.values_text.place(x = 210, y = 50, width = 150, height = 300)
+    self.values_text.configure(state = "disabled")
+    
+    self.quit_button = ttk.Button(self, text="Quit", command = self.quit)
+    self.quit_button.place(x = 650, y = 330, height = 30, width = 95)
+  
+  def quit(self):
+    self.place_forget()
 
+
+tmhelper = TMhelper()

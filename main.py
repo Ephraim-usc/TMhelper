32b25bb456141a9548ca442d5019f98262f02bc5
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-07-03 10:56:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -221,8 +221,7 @@ class Buyer(tk.Frame):
     self.bankcard_text.insert("1.0", self.bc.str())
   
   def submit(self):
-    buyer = op.buyer(self.gm, self.ad, self.bc)
-    op.open_buyer_confirm(buyer)
+    op.open_buyer_confirm(self.gm, self.ad, self.bc)
     self.refresh()
   
   def quit(self):

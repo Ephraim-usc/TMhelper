50e1ccb49486a6067728782d3001ad909d3e35de
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-07-07 10:58:34 -0400

    Update operations.py

--- operations.py
+++ operations.py
@@ -340,11 +340,11 @@ class order(entry):
     br = buyer.query(self.buyer)
     num = br.num_orders()
     current = dt.datetime.now()
-    if self.rank == 2 and num >= 4:
+    if self.get("rank") == 2 and num >= 4:
       fourthorder = buyer.query(br.orders[3])
       if current > fourthorder.get("OrderTime") + TIME_INTERVAL_3:
         return True
-    if self.rank == 3:
+    if self.get("rank") == 3:
       secondorder = buyer.query(br.orders[1])
       srt = secondorder.get("ReviewTime")
       if srt != None and current > srt + TIME_INTERVAL_4:

ef38b0533b548d8d73902897924ea69e2bac31eb
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-07-11 09:29:54 -0400

    Update report.py

--- report.py
+++ report.py
@@ -1,3 +1,12 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
+root = tk.Tk()
+root.geometry("800x500")
+
+frame = tk.Frame(root)
+frame.configure(width = 800, height = 470)
+frame.place(x = 0, y = 30)
+
+start_text = tk.Text(frame)
+start_text.place(x = 50, y = 50, width = 200)

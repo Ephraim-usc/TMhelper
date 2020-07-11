12d011e3f4820e002d4e7fbd7996898e57f76457
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-07-11 10:11:54 -0400

    Update report.py

--- report.py
+++ report.py
@@ -5,6 +5,42 @@ import datetime as dt
 root = tk.Tk()
 root.geometry("800x500")
 
+class Frame(tk.Frame):
+  def __init__(self, parent, *args, **kwargs):
+    tk.Frame.__init__(self, parent, *args, **kwargs)
+    self.parent = parent
+    self.configure(width = 800, height = 470)
+    self['bg'] = 'grey'
+  
+  def copy(self, event):
+    self.parent.clipboard_clear()
+    self.parent.clipboard_append(event.widget.get("1.0", "end-1c"))
+  
+  def clear(self):
+    children = self.winfo_children()
+    children_text = [w for w in children if type(w) == tk.Text]
+    for w in children_text:
+      if w['state'] == 'normal':
+        w.delete("1.0", "end")
+      if w['state'] == 'disabled':
+        w.configure(state = 'normal')
+        w.delete("1.0", "end")
+        w.configure(state = 'disabled')
+  
+  def display(self, widget, string):
+    if widget['state'] == 'normal':
+      widget.delete("1.0", "end")
+      widget.insert("1.0", str(string))
+    if widget['state'] == 'disabled':
+      widget.configure(state = "normal")
+      widget.delete("1.0", "end")
+      widget.insert("1.0", str(string))
+      widget.configure(state = "disabled")
+  
+  def quit(self):
+    self.place_forget()
+
+
 class Report(tk.Frame):
   def __init__(self, *args, **kwargs):
     tk.Frame.__init__(self, *args, **kwargs)
@@ -19,23 +55,21 @@ class Report(tk.Frame):
     
     self.refresh_button = tk.Button(self, text = "Refresh", command = self.refresh)
     self.refresh_button.place(x = 570, y = 50, width = 80, height = 20)
-    
+  
+  def init(self):
+    now = dt.datetime.now(); now = now - dt.timedelta(microseconds = now.microsecond)
+    month_ago = now - dt.timedelta(months = 1)
+    self.clear()
+    self.display(self.start_text, str(month_ago))
+    self.display(self.end_text, str(now))
+  
   def refresh(self):
-      start = self.start_text.get("1.0", "end-1c")
-      end = self.end_text.get("1.0", "end-1c")
-      start = dt.datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
-      print(start)
+    start = self.start_text.get("1.0", "end-1c")
+    end = self.end_text.get("1.0", "end-1c")
+    start = dt.datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
+    print(start)
 
 reportframe = Report(root)
 
-
-
-
-
-
-
-
-now = dt.datetime.now(); now = now - dt.timedelta(microseconds = now.microsecond)
-dt.datetime.strptime("2018-3-12", "%Y-%m-%d")
 
 

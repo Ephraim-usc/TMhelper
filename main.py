9f7645dc7baefa61fc321cd4aae176f4d53d84be
Ephraim-usc <58720699+Ephraim-usc@users.noreply.github.com>
2020-07-27 10:15:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -325,7 +325,7 @@ class Report(Frame):
     self.tree.place(x = 50, y = 100)
     
     treeScroll = ttk.Scrollbar(self, orient="vertical", command = self.tree.yview)
-    treeScroll.place(x = 952, y = 101, height = 322)
+    treeScroll.place(x = 952, y = 101, height = 323)
     self.tree.configure(yscrollcommand = treeScroll.set)
     
     self.tree["columns"] = self.columns
